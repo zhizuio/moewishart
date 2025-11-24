99b504862e1ed5726d7cb0dd577d1dce4fa044a5
zhizuio <zhi.zhao@medisin.uio.no>
2025-11-24 22:53:38 +0100

    add X in `simData.R` and update simulation_doc

--- R/simData.R
+++ R/simData.R
@@ -9,6 +9,8 @@
 #'
 #' @param n number of observations
 #' @param p dimension of the Wishart distribution
+#' @param K number of latent components
+#' @param Xq number of covariates for modeling subpopulation probabilities
 #' @param pis vector of probabilities of subpopulations
 #' @param nus vector of degrees of freedom of Wishart distributions
 #' @param Sigma list of scale matrices of the Wishart distribution
@@ -18,6 +20,7 @@
 #' \item "\code{S}" - a list of covariance matrices
 #' \item "\code{z}" - a vector of probabilities
 #' \item "\code{Sigma_list}" - a list of scale matrices of the Wishart distribution
+#' \item "\code{X}" - a matrix or NULL of covariates
 #' }
 #'
 #'
@@ -30,14 +33,35 @@
 #'
 #' @export
 simData <- function(n = 200, p = 2,
+                    Xq = 0, K = NA,
                     pis = c(0.4, 0.6),
                     nus = c(8, 12),
                     Sigma = NULL) {
   # number of latent components
-  K <- length(pis)
+  if (Xq == 0) {
+    K <- length(pis)
+  } else {
+    if (is.na(K)) {
+      stop("Argument 'K' must be specified!")
+    }
+  }
 
   if (length(nus) != K) {
     stop("Arguments 'pis' and 'nus' must have the same length!")
+  }
+
+  # simulate covarites
+  X <- NULL
+  if (Xq > 0) {
+    pis <- matrix(NA, nrow = n, ncol = K)
+    betas <- matrix(runif(Xq * K, -2, 2), nrow = Xq, ncol = K)
+    betas[, K] <- 0
+    X <- matrix(rnorm(n * Xq), nrow = n, ncol = Xq)
+    expXb <- exp(X %*% betas)
+    sumExpXb <- rowSums(expXb)
+    pis <- apply(expXb, 2, function(xb_k) xb_k / sumExpXb)
+  } else {
+    pis <- matrix(pis, nrow = n, ncol = K, byrow = TRUE)
   }
 
   # define the scale matrix of the Wishart distribution
@@ -65,12 +89,14 @@ simData <- function(n = 200, p = 2,
   S_list <- vector("list", n)
   z_true <- integer(n)
   for (i in 1:n) {
-    k <- sample.int(K, 1, prob = pis)
+    k <- sample.int(K, 1, prob = pis[i, ])
     z_true[i] <- k
     # sample S ~ Wishart(nu_k, Sigma_k) using rWishart
     W <- rWishart(1, df = nus[k], Sigma = Sigma_list[[k]])[, , 1]
     S_list[[i]] <- W
   }
 
-  list(S = S_list, z = z_true, Sigma_list = Sigma_list)
+  list(S = S_list, z = z_true, 
+       nu = nus, pi = pis,
+       Sigma_list = Sigma_list, X = X)
 }
